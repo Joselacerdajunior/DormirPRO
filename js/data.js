efb038e500eebe66aaea5f72c8fffde546ad916b
José Lacerda Junior <jose.lacerdajunior@hotmail.com>
2024-06-07 20:57:07 -0300

    header, plansm help, profile and exit

--- js/data.js
+++ js/data.js
@@ -1,1 +1,239 @@
-var JSONConfig = {};
+// Valores padrão de fábrica
+const JSONConfigDefault = {
+  users: [
+    {
+      user: 'admin', 
+      email: 'admin@dormirpro.com', 
+      password: 'admin',
+
+      other: {
+        configuration: [],
+        modules: [
+          {local: 'Quarto João', status: true, model: 'Berço com balanço'},
+          {local: 'Quarto Maria', status: false, model: 'Berço com balanço e câmera térmica'},
+        ],
+        reports: [],
+      }
+    },
+  ],
+  connectedUserEmail: null,
+};
+
+function loadConfig() {
+  const savedConfig = localStorage.getItem('JSONConfig');
+  if (savedConfig) {
+    try {
+      return JSON.parse(savedConfig);
+    } catch (e) {
+      console.error("Erro ao analisar o JSON salvo:", e);
+      return JSONConfigDefault;
+    }
+  } else {
+    return JSONConfigDefault;
+  }
+}
+
+var JSONConfig = loadConfig();
+
+function saveConfig() {
+  localStorage.setItem('JSONConfig', JSON.stringify(JSONConfig));
+}
+
+if (localStorage.getItem('JSONConfig') === null) {
+  saveConfig();
+}
+
+function atualizarJSONNavegador() {
+  localStorage.setItem('JSONConfig', JSON.stringify(JSONConfig));
+}
+
+
+
+
+
+
+function editarUsuario(email, newUserData) {
+  const userIndex = JSONConfig.users.findIndex(user => user.email === email);
+  if (userIndex !== -1) {
+    JSONConfig.users[userIndex] = { ...JSONConfig.users[userIndex], ...newUserData };
+    console.log(JSONConfig);
+  } else {
+    console.log("Usuário não encontrado");
+  }
+  //editarUsuario('jane.doe@example.com', { user: 'jane_doe', password: 'new_password' });
+  atualizarJSONNavegador();
+}
+
+function adicionarUsuario() {
+  let user = document.getElementById('login').value;
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  let password2 = document.getElementById('password2').value;
+
+  if (password !== password2) {
+    alert("As senhas devem ser iguais!");
+    JSONConfig.connectedUserEmail = null;
+    return;
+  }
+
+  if (password === '') {
+    alert("As senhas devem ser preenchidas!");
+    JSONConfig.connectedUserEmail = null;
+    return;
+  }
+
+  JSONConfig.users.push(
+    {user: `${user}`, email: `${email}`, password: `${password}`},
+  );
+
+  JSONConfig.connectedUserEmail = email;
+
+  atualizarJSONNavegador();
+
+  return true;
+}
+
+function logarUsuario() {
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+
+  const user = JSONConfig.users.find(user => user.email === email && user.password === password);
+
+  if (!user) {
+    alert("Email ou senha incorretos!");
+    JSONConfig.connectedUserEmail = null;
+    return false;
+  }
+
+  JSONConfig.connectedUserEmail = email;
+
+  atualizarJSONNavegador();
+  
+  return true;
+}
+
+function disconnectUser() {
+    JSONConfig.connectedUserEmail = null;
+    
+    atualizarJSONNavegador();
+
+    location.reload();
+}
+
+function iAmLogged() {
+  if (JSONConfig.connectedUserEmail !== null) {
+    return JSONConfig.connectedUserEmail;
+  }
+  return false;
+}
+
+function getLoggedData(email) {
+  const user = JSONConfig.users.find(user => user.email === email);
+  if (user) {
+    return user;
+  } else {
+    return null; // Retorna null se o usuário não for encontrado
+  }
+}
+
+function especificValue(item) {
+  let data = getLoggedData(iAmLogged());
+
+  if (data === null) { return; }
+
+  const keys = item.split('.'); // Divide o caminho do item em partes
+  return keys.reduce((acc, key) => acc && acc[key], data);
+}
+
+function updateUser(email, updatedData) {
+  const userIndex = JSONConfig.users.findIndex(user => user.email === email);
+  if (userIndex !== -1) {
+    const existingUser = JSONConfig.users[userIndex];
+    JSONConfig.users[userIndex] = { 
+      ...existingUser, 
+      ...updatedData,
+      other: {
+        ...existingUser.other,
+        ...updatedData.other
+      }
+    };
+    saveConfig(); // Salvar as atualizações no localStorage
+    console.log("Usuário atualizado:", JSONConfig.users[userIndex]);
+  } else {
+    console.log("Usuário não encontrado");
+  }
+}
+
+
+function updateProfile() {
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let oldPassword = document.getElementById('old-password').value;
+  let newPassword = document.getElementById('new-password').value;
+  let newPassword2 = document.getElementById('new-password2').value;
+
+  if (newPassword !== newPassword2) {
+    alert("Preencha as senhas corretamente!");
+    return;
+  }
+
+  updateUser(iAmLogged(), { user: `${name}`, email: `${email}` });
+
+  if (newPassword !== '') {
+    updateUser(iAmLogged(), { password: `${newPassword}` });
+  }
+
+  loadPage(lastPage);
+}
+
+// Função para editar o módulo
+function editModule(index) {
+  let module = modules[index];
+  console.log('Editando módulo:', module);
+  // Adicione aqui o código para editar o módulo
+}
+
+// Função para remover o módulo
+function removeModule(index) {  
+  let modules = especificValue('other.modules');
+  modules.splice(index, 1);
+  console.log('Módulo removido:', index);
+  // Atualize a exibição da tabela após a remoção
+  saveConfig();
+  renderModules();
+}
+
+// Função para renderizar os módulos novamente
+function renderModules() {
+  let container = document.getElementById('td-container');
+  let modules = especificValue('other.modules');
+  container.innerHTML = '';
+  modules.forEach((element, index) => {
+      container.innerHTML += `
+      <tr>
+          <td>${element['local']}</td>
+          <td class="fixed-width">${(element['status'] === true ? '✔' : '❌')}</td>
+          <td>${element['model']}</td>
+          <td class="fixed-width"><img src="../DormirPRO/assets/editar.png" onerror="this.onerror=null;this.src='../assets/editar.png'" alt="Editar" onclick="editModule(${index})"></td>
+          <td class="fixed-width"><img src="../DormirPRO/assets/lixo.png" onerror="this.onerror=null;this.src='../assets/lixo.png'" alt="Excluir" onclick="removeModule(${index})"></td>
+      </tr>
+      `;
+  });
+}
+
+
+
+
+
+
+function redirecionarRegistro() {
+  if (adicionarUsuario() === true) {
+    loadPage('home', isComponent=false, header='header', footer='footer');
+  }  
+}
+
+function redirecionarLogin() {
+  if (logarUsuario() === true) {
+    loadPage('home', isComponent=false, header='header', footer='footer');
+  }  
+}
--- js/script.js
+++ js/script.js
@@ -1,80 +1,89 @@
+let lastPage = null;
 function loadPage(page, isComponent = false, header = null, footer = null) {
     const content = document.getElementById('content');
     let path = isComponent === true ? 'components' : 'pages';
+    let promises = [];
+
+    lastPage = page;
 
     // Carregar a página principal
-    fetch(`${path}/${page}.html`)
-        .then(response => response.text())
-        .then(data => {
-            content.innerHTML = data;
-        })
-        .catch(error => {
-            content.innerHTML = '<p>Erro ao carregar a página.</p>';
-            console.error('Erro:', error);
-        });
+    promises.push(
+        fetch(`${path}/${page}.html`)
+            .then(response => response.text())
+            .then(data => {
+                content.innerHTML = data;
+            })
+            .catch(error => {
+                content.innerHTML = '<p>Erro ao carregar a página.</p>';
+                console.error('Erro:', error);
+            })
+    );
 
     // Carregar e adicionar o cabeçalho
     if (header !== null) {
-        fetch(`components/${header}.html`)
-            .then(response => response.text())
-            .then(data => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(data, 'text/html');
-                const headerContent = doc.body.childNodes;
-                const fragment = document.createDocumentFragment();
-                headerContent.forEach(node => fragment.appendChild(node));
+        promises.push(
+            fetch(`components/${header}.html`)
+                .then(response => response.text())
+                .then(data => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(data, 'text/html');
+                    const headerContent = doc.body.childNodes;
+                    const fragment = document.createDocumentFragment();
+                    headerContent.forEach(node => fragment.appendChild(node));
 
-                const app = document.getElementById('app');
-                app.insertBefore(fragment, app.firstChild);
-            })
-            .catch(error => {
-                const errorElement = document.createElement('p');
-                errorElement.textContent = 'Erro ao carregar o cabeçalho.';
-                const app = document.getElementById('app');
-                app.insertBefore(errorElement, app.firstChild);
-                console.error('Erro:', error);
-            });
+                    const app = document.getElementById('app');
+                    app.insertBefore(fragment, app.firstChild);
+                })
+                .catch(error => {
+                    const errorElement = document.createElement('p');
+                    errorElement.textContent = 'Erro ao carregar o cabeçalho.';
+                    const app = document.getElementById('app');
+                    app.insertBefore(errorElement, app.firstChild);
+                    console.error('Erro:', error);
+                })
+        );
     }
 
     // Carregar e adicionar o rodapé
     if (footer !== null) {
-        fetch(`components/${footer}.html`)
-            .then(response => response.text())
-            .then(data => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(data, 'text/html');
-                const footerContent = doc.body.childNodes;
-                const fragment = document.createDocumentFragment();
-                footerContent.forEach(node => fragment.appendChild(node));
+        promises.push(
+            fetch(`components/${footer}.html`)
+                .then(response => response.text())
+                .then(data => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(data, 'text/html');
+                    const footerContent = doc.body.childNodes;
+                    const fragment = document.createDocumentFragment();
+                    footerContent.forEach(node => fragment.appendChild(node));
 
-                document.getElementById('app').appendChild(fragment);
-            })
-            .catch(error => {
-                const errorElement = document.createElement('p');
-                errorElement.textContent = 'Erro ao carregar o rodapé.';
-                document.getElementById('app').appendChild(errorElement);
-                console.error('Erro:', error);
-            });
+                    document.getElementById('app').appendChild(fragment);
+                })
+                .catch(error => {
+                    const errorElement = document.createElement('p');
+                    errorElement.textContent = 'Erro ao carregar o rodapé.';
+                    document.getElementById('app').appendChild(errorElement);
+                    console.error('Erro:', error);
+                })
+        );
     }
+
+    // Executar runPageFunctions após todas as promessas serem resolvidas
+    Promise.all(promises).then(() => runPageFunctions(page));
 }
 
-function initializeApp() {
-    //return loadPage('home', isComponent=false, header='header', footer='footer');
-    //return loadPage('login');
-    
-    loadPage('start');
 
-    setTimeout(() => {
-        const startContent = document.getElementById('start-content');
-        const startContainer = document.getElementById('start-container');
-        startContent.classList.remove('animate__fadeIn');
-        startContent.classList.add('animate__fadeOut');
-        startContainer.classList.add('animate__fadeOut');
-    }, 3.3 * 1000);
+function switchProfileVisibility(bool) {
+    let div = document.getElementById('profile-div');
+    let div2 = document.getElementById('profile-out-click-detector');
 
-    setTimeout(() => {
-        loadPage('login');
-    }, 4 * 1000);
+    if (bool === false) {
+        div.classList.add('hidden-pro');
+        div2.classList.add('hidden-pro');
+        return;
+    }
+
+    div.classList.toggle('hidden-pro');
+    div2.classList.toggle('hidden-pro');
 }
 
 function changeNavMenu(expand) {
@@ -124,6 +133,101 @@ function changeNavMenu(expand) {
         break;
     }
 }
+
+function changePasswordVisibility(id) {
+    let input = document.getElementById(id);
+
+    switch (input.type) {
+        case 'text':
+            input.type = 'password';
+            break;
+
+        case 'password':
+            input.type = 'text';
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function capitalizeFirstLetter(string, first=false) {
+    let text = string.charAt(0).toUpperCase() + string.slice(1);
+    
+    if (first === false) {
+        return text;
+    } else {
+        return text.split(' ')[0];
+    }
+}
+
+
+function runPageFunctions(page) {
+    switch (page) {
+        case 'login':
+            if (iAmLogged()) {loadPage('home', isComponent=false, header='header', footer='footer')}
+            break;
+
+        case 'home':
+            document.getElementById('nome-usuario-header').textContent = capitalizeFirstLetter(especificValue('user'), true);
+
+            document.getElementById('name').value = especificValue('user');
+            document.getElementById('email').value = especificValue('email');
+            document.getElementById('old-password').value = especificValue('password');
+            break;
+
+        case 'profile':
+            document.getElementById('nome-usuario-header').textContent = capitalizeFirstLetter(especificValue('user'), true);
+
+            document.getElementById('name').value = especificValue('user');
+            document.getElementById('email').value = especificValue('email');
+            document.getElementById('old-password').value = especificValue('password');
+            break;
+
+        case 'modules':
+            document.getElementById('nome-usuario-header').textContent = capitalizeFirstLetter(especificValue('user'), true);
+
+            let container = document.getElementById('td-container');            
+            let modules = especificValue('other.modules');
+            modules.forEach((element, index) => {
+                container.innerHTML += `
+                <tr>
+                    <td>${element['local']}</td>
+                    <td class="fixed-width">${(element['status'] === true ? '🟢' : '🟠')}</td>
+                    <td>${element['model']}</td>
+                    <td class="fixed-width"><img src="../DormirPRO/assets/editar.png" onerror="this.onerror=null;this.src='../assets/editar.png'" alt="Editar" onclick="editModule(${index}); renderModules();"></td>
+                    <td class="fixed-width"><img src="../DormirPRO/assets/lixo.png" onerror="this.onerror=null;this.src='../assets/lixo.png'" alt="Excluir" onclick="removeModule(${index}); renderModules();"></td>
+                </tr>
+                `;
+            });
+
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function initializeApp() {
+    //return loadPage('modules', isComponent=false, header='header', footer='footer');
+    //return loadPage('login');
+    
+    loadPage('start');
+
+    setTimeout(() => {
+        const startContent = document.getElementById('start-content');
+        const startContainer = document.getElementById('start-container');
+        startContent.classList.remove('animate__fadeIn');
+        startContent.classList.add('animate__fadeOut');
+        startContainer.classList.add('animate__fadeOut');
+    }, 3.3 * 1000);
+
+    setTimeout(() => {
+        loadPage('login');
+    }, 4 * 1000);
+}
+
+//console.log = () => {}
 
 // Carregar a página inicial
 document.addEventListener('DOMContentLoaded', initializeApp);
